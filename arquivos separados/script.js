d7304d7e9f453ff88ddb04879705ea097ea6d0da
marcusgajozo <marcusgajozo@gmail.com>
2022-03-11 15:57:19 -0400

    HTML e Javascript em arquivos separados

---
+++ arquivos separados/script.js
@@ -0,0 +1,12 @@
+var num = document.getElementById('txtn1')
+//var num2 = document.getElementById('txtn2')
+//tem como fazer de outro jeito
+var num2 = document.querySelector('input#txtn2')
+var resultado = document.getElementById('res')
+var soma
+function somar(){
+    soma = Number(num.value) + Number(num2.value)
+    resultado.innerHTML = `A soma entre ${num.value} e ${num2.value} é <strong>${soma}</strong>`
+    num.value=''
+    num2.value=''
+}
