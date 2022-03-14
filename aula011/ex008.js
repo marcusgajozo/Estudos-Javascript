59634f7a15a99877327785638bd4339e5bd8679a
marcusgajozo <marcusgajozo@gmail.com>
2022-03-14 10:52:09 -0400

    aula011 do curso em video

---
+++ aula011/ex008.js
@@ -0,0 +1,6 @@
+var car = 78
+console.log(`A velocidade do seu carro é ${car}km/h`)
+if (car > 60){ //condição simples! pq não tem else
+    console.log('Você ultrapassou a velocidade permitida. Multado!')
+}
+console.log('Dirija sempre usando cinto de segurança')
