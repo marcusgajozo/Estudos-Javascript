2ae8b804b5935d61a703e2b9129856d5fbe5ab46
marcusgajozo <marcusgajozo@gmail.com>
2022-03-17 08:18:54 -0400

    ex014 atualizado a indentação

--- aulaExercicios/ex014/script.js
+++ aulaExercicios/ex014/script.js
@@ -1,13 +1,15 @@
-var div = document.querySelector('div#res');
-var divImg = document.querySelector('div#png')
-var resultadoHora = new Date().getHours();
-div.innerHTML = `<u><strong>Agora são ${resultadoHora} horas</strong></u>`
-if(resultadoHora < 12){
-    divImg.innerHTML = '<img src="img/manha.png" alt="imagem de manhã">';
-} else if (resultadoHora < 18) {
-    divImg.innerHTML = '<img src="img/tarde.png" alt="imagem de tarde">';
-    document.body.style.backgroundColor = 'rgb(84, 116, 143)';
-} else{
-    divImg.innerHTML = '<img src="img/noite.png" alt="imagem de noite">';
-    document.body.style.backgroundColor ='rgb(51, 72, 88)';
+function carregar(){
+    var div = document.querySelector('div#res');
+    var foto = document.querySelector('img#foto')
+    var hora = //new Date().getHours();
+    div.innerHTML = `<u><strong>Agora são ${hora} horas</strong></u>`
+    if(hora < 12){
+        foto.src = 'img/manha.png'
+    } else if (hora < 18) {
+        foto.src = 'img/tarde.png'
+        document.body.style.backgroundColor = 'rgb(84, 116, 143)';
+    } else{
+        foto.src = 'img/noite.png';
+        document.body.style.backgroundColor ='rgb(51, 72, 88)';
+    }
 }
