792e02f43ece0fc4bc1251a3aef2b0d32dc7bfa5
marcusgajozo <marcusgajozo@gmail.com>
2022-03-21 17:55:57 -0400

    ex017 do curso em video

--- aula14ex/ex017/script.js
+++ aula14ex/ex017/script.js
@@ -1,1 +1,15 @@
-alert('Teste')
+function tabuada(){
+    let num = document.querySelector('input#num').value;
+    let tab = document.querySelector('select#seltab')
+    if (num == ''){
+        alert('Por favor, digite um número!')
+    }else{
+        num = Number(num);
+        for(let i = 0; i <= 10; i++){
+            console.log(`${num}x${i} = ${num*i}`)
+        }
+    }
+    
+
+
+}
